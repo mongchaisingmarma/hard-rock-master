d7c12ae599e8bc101dd546c28f3e68157cb1471f
mongchaisingmarma <m.marma41413mongchaising@yahoo.com>
2020-09-01 14:13:23 +0600

    rock-hard-master done

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-// https://api.lyrics.ovh/suggest/baby
+// https://api.lyrics.ovh/suggest/${input}
 // https://api.lyrics.ovh/v1/artist/title
 
 const searchBox = document.getElementById("search-box").addEventListener("click", function () {
     const inputBox = document.getElementById("input-box");
-    // alert(inputBox.value);
     getLyricsReport(inputBox.value);
 });
 
@@ -16,23 +15,27 @@ function getLyricsReport(input) {
 
 // show song report
 function showLyricsReport(data) {
-    // console.log(data);
     const musicDetails = document.getElementById("music-details");
-    const arrData = data.data;
+    let arrData = data.data;
+     arrData = arrData.slice(0, 10);
+    musicDetails.innerHTML = "";
     for (let i = 0; i < arrData.length; i++) {
         const music = arrData[i];
-        console.log(music);
+        let title  = music.title;
+        let artist = music.artist.name;
         const p = document.createElement("p");
         p.innerHTML = `
+        <div class="search-result col-md-8 mx-auto py-4">
             <div class="single-result row align-items-center my-3 p-3">
             <div class="col-md-9">
-                <h3 class="lyrics-name">${music.album.title}</h3>
-                <p class="author lead">Album by <span>${music.artist.name}</span></span></p>
+                <h3 class="lyrics-name">${title}</h3>
+                <p class="author lead">Album by <span>${artist}</span></span></p>
             </div>
             <div class="col-md-3 text-md-right text-center">
-                <button onclick="getLyrics(${music.artist.name}, ${music.album.title})" class="btn btn-success">Get Lyrics</button>
+                <button onclick="getLyrics('${artist}', '${title}')" class="btn btn-success">Get Lyrics</button>
             </div>
             </div>
+        </div>
         `;
         musicDetails.appendChild(p);
     }
@@ -40,7 +43,17 @@ function showLyricsReport(data) {
 
 // music lyrics
 function getLyrics(artist, title) {
-    fetch(`https://api.lyrics.ovh/v1/${music.artist.name}/${music.album.title}`)
+    fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            const arrLyrics = data.lyrics;
+            if (arrLyrics === undefined) {
+                alert("Lyrics is not found");
+            }
+            else{const lyrics = document.getElementById("lyrics");
+            const p = document.createElement("p");
+            p.innerText = arrLyrics;
+            lyrics.appendChild(p);}
+            
+        })
 }
